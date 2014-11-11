01e8e889a6cdb14ced16b68c4f1c0fb61f698d68
Daniel Sweet <dsweet04@gmail.com>
2014-11-11 18:03:11 -0500

    #13 Trying to get travis ci running

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,29 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jasmine : {
       // Your project's source files
-      src : 'ui/public/**/*js',
-      // Your Jasmine spec files
-      specs : 'ui/public/spec/**/*spec.js',
-      // Your spec runner location
-      host: 'http://localhost:63342/cikl/ui/public/SpecRunner.html'
+      src: [
+        'app.js',
+        'controllers/*.js',
+        'directives/*.js',
+        'services/*.js'
+      ],
+      options: {
+        // Your Jasmine spec files
+        specs: 'spec/**/*Spec.js',
+        // Your spec runner location
+        //host: 'http://localhost:63342/cikl/ui/public/SpecRunner.html'
+        vendor: [
+          'js/angular.js',
+          'js/angular-mocks.js',
+          'js/angular-animate.js',
+          'js/angular-resources.js',
+          'js/angular-route.js',
+          'js/bootstrap.js',
+          'js/jquery-2.1.1.js',
+          'js/moment.js',
+          'js/ui_bootstrap-tpls-0.11.0.js'
+        ]
+      }
     }
   });
 
